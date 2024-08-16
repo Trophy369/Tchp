f34673151b14b5fdbff13702f15c83cec09cd2bc
wand3 <wiresharkhack@protonmail.com>
2024-08-16 11:21:49 +0100

    __pycache__ fix

--- tchshop_backend/webapp/auth/views.py
+++ tchshop_backend/webapp/auth/views.py
@@ -9,6 +9,7 @@ from flask_login import login_user, logout_user, login_required
 from models.user import User
 from webapp import db
 
+
 @auth_views.route('/signup', methods=['POST'], strict_slashes=False)
 def signup():
     data = request.json
@@ -16,7 +17,7 @@ def signup():
     # Check if email already exists
     existing_user = User.query.filter_by(email=data['email']).first()
     if existing_user:
-<<<<<<< HEAD
+
         return jsonify({'message': 'Username or email already exists'}), 400
 
     else:
@@ -39,29 +40,6 @@ def signup():
         db.session.commit()
 
         return jsonify({'message': 'User created successfully', 'user': new_user.firstname}), 201
-=======
-        return jsonify({'message': 'Email already exists'}), 400
-
-    new_user = User(
-        firstname=data.get('firstname', ''),
-        lastname=data.get('lastname', ''),
-        email=data['email'],
-        city=data.get('city', ''),
-        state=data.get('state', ''),
-        country=data.get('country', ''),
-        zipcode=data.get('zipcode', ''),
-        street=data.get('street', ''),
-        phone=data.get('phone', ''),
-        agree=True
-    )
-    
-    new_user.set_password(data['password'])
-    
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'User created successfully', 'user': new_user.email}), 201
->>>>>>> refs/remotes/origin/main
 
 
 @auth_views.route('/signin', methods=['GET', 'POST'], strict_slashes=False)
@@ -76,11 +54,8 @@ def signin():
             # token = request.cookies
             logging.info(f"token:{user}")
             session["userId"] = user.id
-<<<<<<< HEAD
-            return jsonify({"Message": "Login Successful", "username": user.firstname, "id": user.id, "roles": len([role.to_dict() for role in user.roles])}), 201
-=======
+
             return jsonify({"Message": "Login Successful", "username": user.firstname, "id": user.id, "roles": len([role.to_dict() for role in user.roles])}), 200
->>>>>>> refs/remotes/origin/main
         return {"error": "Login failed Wrong password"}, 401
     return jsonify({"error": "Login failed"}), 401
 
