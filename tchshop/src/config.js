34b843fed280c824804bb6b6a40ac89b806adfbe
bentocake <philipijohn1990@gmail.com>
2024-09-01 23:21:38 -0400

    hot

--- tchshop/src/config.js
+++ tchshop/src/config.js
@@ -1,5 +1,11 @@
 const config = {
-    baseUrl : "http://47.88.85.77",
+    baseUrl : "http://127.0.0.1:1440",
 }
 
 export default config;
+
+// const config = {
+//     baseUrl : "http://47.88.85.77",
+// }
+
+// export default config;
